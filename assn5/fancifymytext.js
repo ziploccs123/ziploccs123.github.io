f3cba5c53dc5b2d5ca18e17a8a6dcf311a4838d0
ziploccs123 <diegoarias-aceves@csus.edu>
2026-03-23 17:41:28 -0700

    interim : incomplete : compiling
    Added the fancy hash and the boring betty label in html but need to add the back end js code still.

--- assn5/fancifymytext.js
+++ assn5/fancifymytext.js
@@ -2,3 +2,17 @@ function makeBigger() {
     alert("Hello, world!");
     document.getElementById("textInput").style.fontSize = "24pt";
 }
+
+function fancifyText() {
+	let text = document.getElementById("textInput");
+
+	if (document.getElementById("fancy").checked) {
+		text.style.fontWeight = "bold";
+		text.style.color = "blue";
+		text.style.textDecoration = "underline";
+	} else {
+		text.style.fontWeight = "normal";
+		text.style.color = "black";
+		text.style.textDecoration = "none";
+	}
+}
